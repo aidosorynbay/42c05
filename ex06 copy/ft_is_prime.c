e843eb63999ee2338f5ea407339a31223f2a0d6e
Aidos Orynbay <aorynbay@lab1r5s8.42abudhabi.ae>
2024-05-12 12:34:23 +0400

    c05

--- ex06 copy/ft_is_prime.c
+++ ex06 copy/ft_is_prime.c
@@ -10,3 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb == 1 || nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 4)
+	{
+		return (0);
+	}
+	while (i < nb / i)
+	{
+		if (nb % i != 0)
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	printf("%i\n", ft_is_prime(17));
+// 	printf("%i\n", ft_is_prime(4));
+// 	printf("%i\n", ft_is_prime(61));
+// }
--- ex07 copy/ft_find_next_prime.c
+++ ex07 copy/ft_find_next_prime.c
@@ -1,64 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/08 19:26:31 by aorynbay          #+#    #+#             */
-/*   Updated: 2024/05/08 19:26:32 by aorynbay         ###   ########.fr       */
+/*   Created: 2024/05/11 15:26:49 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/11 15:26:50 by aorynbay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	checking_midpoint(int num)
+int	next_prime(int num)
 {
-	int		min;
-	long	mid;
-	int		max;
+	int	i;
 
-	min = 1;
-	max = num;
-	while (max - 1 > min)
-	{
-		mid = (max + min) / 2;
-		if (mid * mid > num)
-		{
-			max = mid;
-		}
-		else if (mid * mid < num)
-		{
-			min = mid;
-		}
-		else
-		{
-			return (mid);
-		}
-	}
-	return (0);
-}
-
-int	ft_sqrt(int nb)
-{
-	int	result;
-
-	result = 0;
-	if (nb == 0 || nb < 0)
+	i = 2;
+	if (num == 1 || num < 0)
 	{
 		return (0);
 	}
-	else if (nb == 1)
+	while (i <= num / i)
 	{
-		return (1);
+		if (num % i != 0)
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
 	}
-	else
+	return (num);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (next_prime(nb) == 0)
 	{
-		result = checking_midpoint(nb);
-		return (result);
+		nb++;
 	}
+	return (nb);
 }
 
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	printf("%i", ft_sqrt(80));
+// 	int a = 4;
+// 	printf("%i", ft_find_next_prime(a));
 // }
